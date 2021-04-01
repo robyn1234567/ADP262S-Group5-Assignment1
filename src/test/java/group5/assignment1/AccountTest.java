8ae2b4b5a5d99c5a6f8a866ac34581f1e68fc464
220126283 <220126283@DESKTOP-AERPNAL.home>
2021-04-01 17:36:35 +0200

    "Added TestAccount"

---
+++ src/test/java/group5/assignment1/AccountTest.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group5.assignment1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 220126283
+ */
+public class AccountTest {
+    
+    public AccountTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getId method, of class Account.
+     */
+    @Test
+    public void testGetId() {
+        System.out.println("getId");
+        Account instance = new Account();
+        String expResult = "";
+        String result = instance.getId();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setId method, of class Account.
+     */
+    @Test
+    public void testSetId() {
+        System.out.println("setId");
+        String id = "";
+        Account instance = new Account();
+        instance.setId(id);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getName method, of class Account.
+     */
+    @Test
+    public void testGetName() {
+        System.out.println("getName");
+        Account instance = new Account();
+        String expResult = "";
+        String result = instance.getName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setName method, of class Account.
+     */
+    @Test
+    public void testSetName() {
+        System.out.println("setName");
+        String name = "";
+        Account instance = new Account();
+        instance.setName(name);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAccountType method, of class Account.
+     */
+    @Test
+    public void testGetAccountType() {
+        System.out.println("getAccountType");
+        Account instance = new Account();
+        String expResult = "";
+        String result = instance.getAccountType();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setAccountType method, of class Account.
+     */
+    @Test
+    public void testSetAccountType() {
+        System.out.println("setAccountType");
+        String accountType = "";
+        Account instance = new Account();
+        instance.setAccountType(accountType);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
