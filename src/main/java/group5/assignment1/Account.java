6ce041bb88c8db7b474676c6a61226cce2aa582d
220126283 <220126283@DESKTOP-AERPNAL.home>
2021-04-01 17:29:08 +0200

    "Added attributes to Account class, removed TestAccount"

--- src/main/java/group5/assignment1/Account.java
+++ src/main/java/group5/assignment1/Account.java
@@ -6,4 +6,29 @@ package group5.assignment1;
  */
 public class Account {
 
+    private String id, name, accountType;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
 }
